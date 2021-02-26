69ac7dc0f6273460762fe6a7804c17ec28970642
Imperial-Industries <shivansh.gour90@gmail.com>
2021-02-26 17:49:41 -0500

    correlation coefficient

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 fn main() {
     let data = [1,2,3,43892];
     let x = (0..data.len()).collect::<Vec<_>>();
-    //let data_avg = avg(&data);
     let _x_avg = avg(&x);
     println!("{:?} {:?}",standard_deviation(&x),coefficient(&x,&data));
 }
@@ -16,16 +15,9 @@ fn standard_deviation(data: &[usize]) -> f64 {
 fn coefficient(x: &[usize], y: &[usize]) -> f64 {
     let x_avg = avg(&x);
     let y_avg = avg(&y);
-    let x_sum:f64 = x.iter().map(|i|(*i as f64-x_avg)).sum();
-    let y_sum:f64 = y.iter().map(|i|(*i as f64-y_avg)).sum();
-    let denominator = x_sum.sqrt() * y_sum.sqrt();
-    let sum:f64 = x.iter().map(|i|(*i as f64-x_avg)*(y[*i] as f64-y_avg)).sum();
-    sum/denominator as f64
+    let x_sum:f64 = x.iter().map(|i|(*i as f64-x_avg)*(*i as f64-x_avg)).sum();
+    let y_sum:f64 = y.iter().map(|i|(*i as f64-y_avg)*(*i as f64-y_avg)).sum();
+    let denominator = (x_sum * y_sum).sqrt();
+    let numerator:f64 = x.iter().map(|i|(*i as f64-x_avg)*(y[*i] as f64-y_avg)).sum();
+    numerator/denominator as f64
 }
-/*
-y = [1,2,3,4]
-x = [0,1,2,3]
-s = 0
-for i in range(len(x)):
-    s += (x[i]-avg(x))*(y[i]-avg(y))
-*/
