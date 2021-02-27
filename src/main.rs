0a4ec684cc39f0cd8e7600ed91beb6f1161d9854
Imperial-Industries <shivansh.gour90@gmail.com>
2021-02-27 00:12:53 -0500

    yayayay

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,51 @@
-fn main() {
-    let data = [1,2,3,43892];
-    let x = (0..data.len()).collect::<Vec<_>>();
-    let _x_avg = avg(&x);
-    println!("{:?} {:?}",standard_deviation(&x),coefficient(&x,&data));
+use plotlib::page::Page;
+use plotlib::repr::Plot;
+use plotlib::view::ContinuousView;
+use plotlib::style::{PointStyle};
+pub fn main() {
+    let data = [0,1,2,3,4,5];
+    let indices = (0..data.len()).collect::<Vec<_>>();
+    let x = [20,25,30,35,40,46];
+    let converted_data: Vec<_> = data.iter().map(|i| *i as f64).collect();
+    let converted_x: Vec<_> = x.iter().map(|i| *i as f64).collect();
+    println!("Mean: {}", avg(&data));
+    println!("Standard Deviation: {}", standard_deviation(&data));
+    println!("Correlation Coefficient: {}", coefficient(&indices, &x, &data));
+    let slope: f64 = (coefficient(&indices, &x, &data) * standard_deviation(&data))/(standard_deviation(&x));
+    match slope as i32 {
+        -1 => println!("Line of Best Fit in Standard Form: y - {} = -(x - {})", avg(&data), avg(&x)),
+        1 => println!("Line of Best Fit in Standard Form: y - {} = x - {}", avg(&data), avg(&x)),
+        _ => println!("Line of Best Fit in Standard Form: y - {} = {}(x - {})", avg(&data), slope, avg(&x))
+    };
+    let dataset = converted_x.into_iter().zip(converted_data.into_iter()).collect::<Vec<_>>();
+    let plot_one: Plot = Plot::new(dataset).point_style(
+        PointStyle::new()
+            .colour("#000000")
+    );
+    let view = ContinuousView::new()
+        .add(plot_one)
+        .x_label("X-Axis")
+        .y_label("Data");
+    Page::single(&view).save("plot.svg").unwrap();
 }
-fn avg(data: &[usize]) -> f64 {
+pub fn avg(data: &[usize]) -> f64 {
     data.iter().sum::<usize>() as f64/data.len() as f64
 }
-fn standard_deviation(data: &[usize]) -> f64 {
+pub fn standard_deviation(data: &[usize]) -> f64 {
     let avg = avg(&data);
-    let sum:f64 = data.iter().map(|i|(*i as f64-avg)*(*i as f64-avg)).sum();
+    let sum:f64 = data.iter().map(|i|(*i as f64-avg) * (*i as f64-avg)).sum();
     (sum/(data.len() as f64 - 1.)).sqrt()
 }
-fn coefficient(x: &[usize], y: &[usize]) -> f64 {
+pub fn coefficient(indices: &[usize], x: &[usize], y: &[usize]) -> f64 {
     let x_avg = avg(&x);
     let y_avg = avg(&y);
-    let x_sum:f64 = x.iter().map(|i|(*i as f64-x_avg)*(*i as f64-x_avg)).sum();
-    let y_sum:f64 = y.iter().map(|i|(*i as f64-y_avg)*(*i as f64-y_avg)).sum();
+    let x_sum:f64 = x.iter().map(|i|(*i as f64-x_avg) * (*i as f64-x_avg)).sum();
+    let y_sum:f64 = y.iter().map(|i|(*i as f64-y_avg) * (*i as f64-y_avg)).sum();
     let denominator = (x_sum * y_sum).sqrt();
-    let numerator:f64 = x.iter().map(|i|(*i as f64-x_avg)*(y[*i] as f64-y_avg)).sum();
+    let numerator:f64 = indices.iter().map(|i|(x[*i] as f64 - x_avg)*(y[*i] as f64 - y_avg)).sum();
     numerator/denominator as f64
 }
+/*
+pub fn plug(slope: &[usize], x: &[usize], y: &[usize], ) -> f64 {
+    
+}*/
